68ef93a14d4fb764f57543b24d4d325eabf195f9
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-02-15 22:49:13 +0200

    About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
@@ -9,8 +10,11 @@ export default class About extends Component<Props, State> {
 
   render() {
     return (
-      <div className="h-screen flex flex-col text-center md:text-left md:flex-row relative">
-        About
+      <div className="h-screen flex flex-col justify-evenly items-center text-center md:text-left md:flex-row relative max-w-7xl mx-auto px-10">
+        <h3 className="absolute top-24 text-2xl text-gray-500 tracking-[20px] uppercase">
+          About
+        </h3>
+        <motion.img src='../assets/images/photo_2022-12-01_13-49-04.png' />
       </div>
     );
   }
