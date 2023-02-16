cba7ae4911413357863d17cd6e319177e1928620
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-02-16 10:13:34 +0200

    adding photos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,24 @@ export default class About extends Component<Props, State> {
         <h3 className="absolute top-24 text-2xl text-gray-500 tracking-[20px] uppercase">
           About
         </h3>
-        <motion.img src='../assets/images/photo_2022-12-01_13-49-04.png' />
+        <motion.img
+          initial={{
+            x: 200,
+            opacity: 0,
+          }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+          }}
+          viewport={{
+            once: true,
+          }}
+          transition={{
+            duration: 0.8,
+          }}
+          src="/static/about-picture.png"
+          className="flex-shrink-0 max-md:rounded-full object-cover md:w-80 md:h-80 -pb-30 max-md:w-60 max-md:h-60 rounded-lg xl:w-[500px] xl:h-[600px]"
+        />
       </div>
     );
   }
