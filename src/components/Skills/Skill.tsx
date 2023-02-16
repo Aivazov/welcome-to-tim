8278fed7a336a3d4f3a4bb6fab013f0da009505a
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-02-16 14:25:55 +0200

    finishing styling Skills section

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -20,7 +20,14 @@ export default function Skill({ directionLeft }: Props) {
         transition={{
           duration: 1,
         }}
+        src="/static/skills/css3.svg"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
+      <div className="absolute opacity-0 group-hover:opacity-80 group-hover:bg-white/80 rounded-full transition duration-300 ease-in-out max-md:w-24 max-md:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-2xl text-black font-bold opacity-100">100%</p>
+        </div>
+      </div>
     </div>
   );
 }
