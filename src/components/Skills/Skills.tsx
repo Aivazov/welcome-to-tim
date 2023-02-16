bb2cbd83368b715613f34541f19d2bcba5185fb0
Tim Aivazov <37597659+Aivazov@users.noreply.github.com>
2023-02-16 13:56:05 +0200

    working on Skills section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Skill from './Skill';
 
 type Props = {};
 
@@ -14,6 +15,17 @@ export default function Skills({}: Props) {
       <h3 className="absolute top-24 ml-4 text-gray-500 text-2xl tracking-[19px] uppercase">
         Skills
       </h3>
+
+      <h3 className="absolute top-36 text-gray-500 text-sm tracking-[3px] uppercase">
+        Some text here
+      </h3>
+
+      <div className="grid grid-cols-4 gap-5">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
     </motion.div>
   );
 }
